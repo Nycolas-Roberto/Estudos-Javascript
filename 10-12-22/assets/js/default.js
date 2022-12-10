debf79b44af3ae20d1aab4c6b3a0903776acc1b9
NycolasSantana <nycolasrobertods16@gmail.com>
2022-12-10 15:55:32 -0300

    Operadores lógicos

--- 10-12-22/assets/js/default.js
+++ 10-12-22/assets/js/default.js
@@ -1,6 +1,6 @@
+/*
 let n1 = 5
 let n2 = 2
-
 document.write(n1+n2  + "<br>");
 document.write(n1-n2  + "<br>");
 document.write(n1*n2  + "<br>");
@@ -8,13 +8,16 @@ document.write(n1/n2  + "<br>");
 document.write(n1%n2  + "<br>");
 document.write(n1**n2 + "<br>");
 document.write((n1+n2)/n1 + "<br>");
-
-
 let n = 10;
 n += 10;
-
 n--
 --n
-
 n++
 ++n
+alert(5>5);
+alert(5<7);
+alert(5>=9);
+alert(5<=8);
+alert(5==5);
+alert(5!=4); 
+*/
