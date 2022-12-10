9adca3e51f7e2a306f3c0f574675695ba9570e17
NycolasSantana <nycolasrobertods16@gmail.com>
2022-12-10 13:03:45 -0300

    Aritméticos

---
+++ 10-12-22/assets/js/default.js
@@ -0,0 +1,10 @@
+let n1 = 5
+let n2 = 2
+
+document.write(n1+n2  + "<br>");
+document.write(n1-n2  + "<br>");
+document.write(n1*n2  + "<br>");
+document.write(n1/n2  + "<br>");
+document.write(n1%n2  + "<br>");
+document.write(n1**n2 + "<br>");
+document.write((n1+n2)/n1 + "<br>");
