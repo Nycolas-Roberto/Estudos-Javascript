d41d11cb78863b0cb2e985db75d111c4ef1bd473
NycolasSantana <nycolasrobertods16@gmail.com>
2022-12-11 23:34:08 -0300

    Condicionais

---
+++ 11-12-22/ex002.js
@@ -0,0 +1,5 @@
+let vel = 60.5;
+console.log(`Velocidade do seu carro é: ${vel}`);
+if (vel > 60) {
+    console.log(`LIMITE DE VELOCIDADE ATINGIGA!`)
+}
---
+++ 11-12-22/ex003.js
@@ -0,0 +1,28 @@
+/*
+let pais = 'brasil'.toUpperCase();
+if (pais != 'BRASIL') {
+    console.log('- Estrangeiro');
+} else {
+    console.log('- Brasileiro');
+}
+*/
+/*
+let idade = 17;
+if (idade >= 18) {
+    console.log('Voto obrigatório');
+} else if (idade >= 16 && idade < 18) {
+    console.log('Voto opcional');
+} else {
+    console.log('Voto negado')
+}
+*/
+/*
+let atual = new Date();
+let horas = atual.getHours();
+let mes = atual.getMonth();
+if (horas == 211) {
+    console.log('Boa Noite');
+} else if (mes == 11) {
+    console.log('Fim de ano')
+}
+*/
